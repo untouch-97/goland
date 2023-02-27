904b37d20fdadc86f7b62e9c7be8ef74dab94c8d
untouch <bahodir5563325@gmail.com>
2023-02-28 01:20:16 +0500

    add test

--- gotour/test/first.go
+++ gotour/test/first.go
@@ -1,1 +1,15 @@
-package test
+package main
+
+import "fmt"
+
+func main() {
+	sayHello()
+	sayHello("Sammy")
+	sayHello("Sammy", "Jessica", "Drew", "Jamie")
+}
+
+func sayHello(names ...string) {
+	for _, n := range names {
+		fmt.Printf("Hello %s\n", n)
+	}
+}
