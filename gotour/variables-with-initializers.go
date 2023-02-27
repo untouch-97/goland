48d2e86440e1f0b098e2191142ba2dae20b97020
untouch <bahodir5563325@gmail.com>
2023-02-27 15:35:51 +0500

    dopolnil variables-with-initializers.go

--- gotour/variables-with-initializers.go
+++ gotour/variables-with-initializers.go
@@ -5,6 +5,12 @@ import "fmt"
 var i, j int = 1, 2
 
 func main() {
-	var c, python, java = true, false, "no!"
-	fmt.Println(c, i, j, python, java)
+	//var c, python, java = true, false, "no!"
+	//fmt.Println(c, i, j, python, java)
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
+
 }
