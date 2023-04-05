dadf7f5febd72ce8625c14543f6a93890eee5721
untouch <Bahodir5563325@gmail.com>
2023-04-05 23:14:13 +0500

    add other/slices/naviki-mentovskayaProgramma/main.go

--- ThisIsIT/other/slices/naviki-mentovskayaProgramma/README.md
+++ ThisIsIT/other/slices/naviki-mentovskayaProgramma/README.md
@@ -1,1 +1,2 @@
+youtube channel
 https://www.youtube.com/watch?v=1vAIvqDo5LE&ab_channel=%D0%9D%D0%B0%D0%B2%D1%8B%D0%BA%D0%B8%E2%80%94%D0%BC%D0%B5%D0%BD%D1%82%D0%BE%D1%80%D1%81%D0%BA%D0%B0%D1%8F%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B0
---
+++ ThisIsIT/other/slices/naviki-mentovskayaProgramma/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
+
+func main() {
+	intSlice := make([]int, 0, 10)
+	for i := 0; i < 10; i++ {
+		intSlice = append(intSlice, i)
+	}
+	fmt.Println("---------------------------")
+	fmt.Printf("| intSlice %v, len: %d, cap: %d \n", intSlice, len(intSlice), cap(intSlice))
+
+	fmt.Println("---------------------------")
+	cut := intSlice[2:4]
+	fmt.Printf("| intSlice %v, len: %d, cap: %d \n", cut, len(cut), cap(cut))
+
+	fmt.Println("---------------------------")
+	cut = cut[:cap(cut)]
+	_ = cut[2]
+	fmt.Printf("| cut %v, len: %d, cap: %d \n", cut, len(cut), cap(cut))
+
+	fmt.Println("---------------------------")
+	fmt.Printf(
+		"| intSlice  %d, cut: %d |\n"+
+			"| intSlice %v, cut: %v |\n",
+		reflect.ValueOf(intSlice).Pointer(),
+		reflect.ValueOf(cut).Pointer(),
+		*(*reflect.SliceHeader)(unsafe.Pointer(&intSlice)),
+		*(*reflect.SliceHeader)(unsafe.Pointer(&cut)),
+	)
+	fmt.Println("---------------------------")
+	cut[0] = 1 << 32
+	fmt.Printf("| intSlice %v, len: %d, cap: %d \n", intSlice, len(intSlice), cap(intSlice))
+	fmt.Printf("| cut %v, len: %d, cap: %d \n", cut, len(cut), cap(cut))
+	fmt.Println("---------------------------")
+
+	cut = append(cut, (1<<32 + 1))
+	cut[0] = 1 << 10
+	fmt.Printf("| intSlice %v, len: %d, cap: %d \n", intSlice, len(intSlice), cap(intSlice))
+	fmt.Printf("| intSlice %v, len: %d, cap: %d \n", cut, len(cut), cap(cut))
+	fmt.Println("---------------------------")
+
+}
